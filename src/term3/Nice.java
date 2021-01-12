5a953e2b1252fd1d1faf0a90aff484be286507f2
wooyoung3864 <71897874+wooyoung3864@users.noreply.github.com>
2021-01-12 15:55:20 +0900

    Nice

---
+++ src/term3/Nice.java
@@ -0,0 +1,10 @@
+package term3;
+import java.util.Scanner;
+public class Nice {
+    public static final Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Enter a number: ");
+        int num = in.nextInt();
+        for(int i = 0; i <= num; ++i) System.out.println("Nice");
+    }
+}
