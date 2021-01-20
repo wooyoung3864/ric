b1b2d0bacdbc099467cd1e12cafcaca85f4bcde8
wooyoung3864 <71897874+wooyoung3864@users.noreply.github.com>
2021-01-20 09:24:56 +0900

    minor changes

--- AP CSA Term 3/src/classwork/LoopsQ.java
+++ AP CSA Term 3/src/classwork/LoopsQ.java
@@ -10,19 +10,17 @@ public class LoopsQ {
     }
 
     public static String oddEvenZero(String num) {
-        int even = 0;
-        int odd = 0;
-        int zero = 0;
-        for(int i = 0; i < num.length(); i++) {
+        int even = 0, odd = 0, zero = 0;
+        for(int i = 0; i < num.length(); ++i) {
             String part = num.substring(i , i+1);
             int digit = Integer.parseInt(part);
 
             if(digit % 2 == 0) {
-                even++;
+                ++even;
             } else if(digit % 2 == 1) {
-                odd++;
+                ++odd;
             } else {
-                zero++;
+                ++zero;
             }
         }
         return "Number of even digits: " + even + "\n" + "Number of odd digits: " + odd + "\n" + "Number of zero digits: " + zero;
