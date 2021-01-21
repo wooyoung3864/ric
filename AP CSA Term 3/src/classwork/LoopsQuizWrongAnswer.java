a89854f1bfaa1dd6aac950b2fc59ba0aa7878c5c
wooyoung3864 <71897874+wooyoung3864@users.noreply.github.com>
2021-01-21 16:20:47 +0900

    minor changes

--- AP CSA Term 3/src/classwork/LoopsQuizWrongAnswer.java
+++ AP CSA Term 3/src/classwork/LoopsQuizWrongAnswer.java
@@ -1,6 +1,6 @@
 package classwork;
 import java.util.Scanner;
-public class LoopsQ {
+public class LoopsQuizWrongAnswer {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter a number: ");
