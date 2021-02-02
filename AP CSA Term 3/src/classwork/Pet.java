aa659def31a8a74208c295597c3d75d942186fe7
wooyoung3864 <71897874+wooyoung3864@users.noreply.github.com>
2021-02-02 14:11:24 +0900

    Q

---
+++ AP CSA Term 3/src/classwork/Pet.java
@@ -0,0 +1,67 @@
+package classwork;
+
+import javax.swing.text.html.parser.TagElement;
+
+public class Pet {
+    private String name;
+    private int age;
+    private double weight;
+    private String type;
+    private static int numInstances;
+
+    public Pet() {
+        name = "Tory";
+        age = 6;
+        weight = 7.4;
+        type = "Dog";
+        ++numInstances;
+    }
+
+    public Pet(String name, int age, double weight, String type) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.type = type;
+        ++numInstances;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public static int getNumInstances() {
+        return numInstances;
+    }
+
+    public String toString() {
+        return "Name: " + name + "\nAge: " + age + "\nWeight: " + weight + "\nType: " + type + "\nNumber of pets on record: " + numInstances;
+    }
+}
---
+++ AP CSA Term 3/src/classwork/PetMain.java
@@ -0,0 +1,10 @@
+package classwork;
+
+public class PetMain {
+    public static void main(String[] args) {
+        Pet tory = new Pet();
+        Pet toto = new Pet("Toto", 3, 5.4, "Dog");
+        System.out.println(tory);
+        System.out.println(toto);
+    }
+}
